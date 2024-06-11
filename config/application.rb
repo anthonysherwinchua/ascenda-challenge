a19f03f7f9d7d5c7ac4adcfa6a571027b5373442
anthonysherwinchua <anthonysherwinchua@gmail.com>
2024-06-11 11:03:21 +0800

    set application to API only

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module AscendaChallenge
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.api_only = true
   end
 end
