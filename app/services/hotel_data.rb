32866f0b66bc431dd712f77db4a11fc7a486e5ab
anthonysherwinchua <anthonysherwinchua@gmail.com>
2024-06-11 16:09:27 +0800

    use ORM of suppliers

--- app/services/hotel_data.rb
+++ app/services/hotel_data.rb
@@ -1,15 +1,31 @@
 class HotelData
   attr_reader :supplier
 
+  ORM_MAPPING = {
+    acme: Orm::Acme,
+    paperflies: Orm::Paperflies,
+    patagonia: Orm::Patagonia
+  }.freeze
+
   def initialize(supplier_id)
     @supplier = Supplier.find(supplier_id)
   end
 
   def call
-
+    if orm
+      data.each do |attributes|
+        orm.new(attributes).save
+      end
+    else
+      Rails.logger.info("ORM not found")
+    end
   end
 
   private
+
+  def orm
+    @orm ||= ORM_MAPPING[supplier.name.to_sym]
+  end
 
   def response
     @response ||= HTTParty.get(supplier.url)
--- app/sidekiq/supplier_data_job.rb
+++ app/sidekiq/supplier_data_job.rb
@@ -5,8 +5,8 @@ class SupplierDataJob
     supplier_ids = Supplier.where(name: supplier_names).pluck(:id)
     supplier_ids = Supplier.pluck(:id) if supplier_ids.blank?
 
-    supplier_ids.each do |supplier|
-      HotelDataJob.perform_later(supplier.id)
+    supplier_ids.each do |supplier_id|
+      HotelDataJob.perform_async(supplier_id)
     end
   end
 end
