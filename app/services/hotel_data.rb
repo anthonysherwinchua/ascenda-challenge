1cc671880abf687aad46e79b49971031b6b5f7cd
anthonysherwinchua <anthonysherwinchua@gmail.com>
2024-06-11 14:31:09 +0800

    fetch the data from supplier

--- app/services/hotel_data.rb
+++ app/services/hotel_data.rb
@@ -5,5 +5,21 @@ class HotelData
     @supplier = Supplier.find(supplier_id)
   end
 
-  def call; end
+  def call
+
+  end
+
+  private
+
+  def response
+    @response ||= HTTParty.get(supplier.url)
+  end
+
+  def data
+    @data ||= JSON.parse(response.body)
+  rescue => e
+    Rails.logger.error("Invalid response: #{e.message}")
+
+    { error: "failed to parse response" }
+  end
 end
